892898bb4c4faec84d5598784a716482738d0784
Peter Okafor <okaforpeterchukwuemeka2005@gmail.com>
2025-03-13 08:45:23 +0100

    update bcryptjs version and standardize import quotes in prisma.ts

--- src/config/db/prisma.ts
+++ src/config/db/prisma.ts
@@ -1,12 +1,12 @@
-import path from 'path';
+import path from "path";
 import { isProd } from "../const";
-import { PrismaClient } from '@prisma/client';
-import { __dirname } from '@/config/const'
+import { PrismaClient } from "@prisma/client";
+import { __dirname } from "@/config/const";
 
 // Set Prisma Query Engine Path (optional)
 process.env.PRISMA_QUERY_ENGINE_BINARY = path.join(
   __dirname,
-  'node_modules/.prisma/client/query-engine-darwin'
+  "node_modules/.prisma/client/query-engine-darwin",
 );
 
 interface CustomNodeJsGlobal extends Global {
