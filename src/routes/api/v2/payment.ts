4496bb32ce4041ab4617244c96f097d733b3303c
iayvob <165709714+iayvob@users.noreply.github.com>
2025-03-31 00:25:09 +0100

    Create the payment needed function for weebhook events handler for subsciptions and payments cases

--- src/routes/api/v2/payment.ts
+++ src/routes/api/v2/payment.ts
@@ -1,27 +1,12 @@
-// Project: Payment
-// Zhouzhou, Backend Intern Team
-
+import { subscriptionHandler, subscriptionPaymentHandler } from "@/controllers/payment-controller";
 import { Router } from "express";
-import {
-  createPayment,
-  getPaymentStatus,
-  processWebhook,
-} from "@/controllers/payment-controller";
-import { createPaymentSchema, getPaymentStatusSchema, processWebhookSchema } from "@/validations/paymentSchema";
-import validate from "express-zod-safe";
 
 const router = Router({ mergeParams: true });
 
-/* Configure the Routes */
-router.post("/createUrl", validate(createPaymentSchema), createPayment);
-
-router.post("/webhook", validate(processWebhookSchema), processWebhook);
-
-router.post("/checkStatus", validate(getPaymentStatusSchema), getPaymentStatus);
-
 // Configure Lemon Squeezy Webhooks
-router.post("/lemon-squeezy/webhooks/subscription-status")
 
-router.post("/lemon-squeezy/webhooks/payment-status")
+router.post("/lemon-squeezy/webhooks/subscription-status", subscriptionHandler)
+
+router.post("/lemon-squeezy/webhooks/payment-status", subscriptionPaymentHandler)
 
 export default router;
